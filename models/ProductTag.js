feaa027b845c1654ed0c28aa4fc0ba7309f7846d
Ahmed Alasow <a.alasow01@gmail.com>
2023-01-08 23:01:03 +0000

    fixed syntax errors

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -9,14 +9,25 @@ ProductTag.init(
     // define columns
 
     id: {
-
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
 
     product_id: {
-      
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product',
+        key: 'id',
+      }
     },
     tag_id: {
-      
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag',
+        key: 'id'
+      }
     }
   },
   {
